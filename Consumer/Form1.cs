dd49f9af219bf19dbde8d136fd0e6132cb5c9613
Abo Bus <egorik2706@gmail.com>
2023-12-01 22:49:43 +0300

    Init Commit

---
+++ Consumer/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Data;
+using System.Drawing;
+using System.Net.Sockets;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace Consumer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.ContextMenuStrip = contextMenuStrip1;
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            int port = int.Parse(ConfigurationManager.AppSettings.Get("port"));
+            UdpClient client = new UdpClient(port);
+
+            while (true)
+            {
+                UdpReceiveResult data = await client.ReceiveAsync();
+                using (MemoryStream ms = new MemoryStream(data.Buffer))
+                {
+                    pictureBox1.Image = new Bitmap(ms);
+                }
+                Text = $"Received bytes: {data.Buffer.Length * sizeof(byte)}";
+            }
+        }
+
+        private void pictureBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                pictureBox1.Image = null;
+                Invalidate();
+                Text = string.Empty;
+            }
+
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+            MessageBox.Show(string.Join("\n", host.AddressList.
+                Where(i => i.AddressFamily == AddressFamily.InterNetwork)), "IPv4");
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string exePath = AppDomain.CurrentDomain.BaseDirectory;
+                string path = Path.Combine(exePath, @"HelpImg\tutorial.png");
+                pictureBox1.Image = new Bitmap(path);
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.ResetColor();
+            }
+        }
+
+        #region Drawing
+        private Point _lastPoint = Point.Empty;
+        private bool _isDragging = false;
+        private readonly Pen _pen = new Pen(Color.Black, 2);
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            _lastPoint = e.Location;
+            _isDragging = true;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_isDragging)
+            {
+                if (pictureBox1.Image == null)
+                {
+                    Bitmap bitMap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                    pictureBox1.Image = bitMap;
+                }
+                using (Graphics graphics = Graphics.FromImage(pictureBox1.Image))
+                {
+                    graphics.DrawLine(_pen, _lastPoint, e.Location);
+                }
+                pictureBox1.Invalidate();
+                _lastPoint = e.Location;
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            _isDragging = false;
+            _lastPoint = Point.Empty;
+        }
+        #endregion
+    }
+}
---
+++ Producer/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Drawing;
+using System.Threading;
+using System.Net.Sockets;
+using System.Configuration;
+using System.Drawing.Imaging;
+using AForge.Video.DirectShow;
+using System.Runtime.InteropServices;
+
+namespace Producer
+{
+    internal class Program
+    {
+        const byte SW_HIDE = 0;
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, byte nCmdShow);
+
+        private static IPEndPoint consumerEndPoint;
+        private static readonly UdpClient udpClient = new UdpClient();
+
+        static void Main(string[] args)
+        {
+            string consumerIP = ConfigurationManager.AppSettings.Get("consumerIP");
+            int consumerPort = int.Parse(ConfigurationManager.AppSettings.Get("consumerPort"));
+            consumerEndPoint = new IPEndPoint(IPAddress.Parse(consumerIP), consumerPort);
+            Console.WriteLine($"Consumer: {consumerEndPoint}");
+
+            Console.WriteLine("\n...Press ENTER to hide the console...");
+            Console.WriteLine("\n...Press ANY OTHER key to exit.......");
+            if (Console.ReadKey(true).Key == ConsoleKey.Enter)
+            {
+                FilterInfoCollection videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+                VideoCaptureDevice videoSource = new VideoCaptureDevice(videoDevices[0].MonikerString);
+                videoSource.NewFrame += VideoSource_NewFrame;
+                videoSource.Start();
+
+                Console.Beep(800, 300);
+                Console.Beep(800, 300);
+                ShowWindow(GetConsoleWindow(), SW_HIDE);
+            }
+            else
+            {
+                Console.Beep(800, 1000);
+                Thread.Sleep(700);
+                Environment.Exit(0);
+            }
+        }
+
+        private static void VideoSource_NewFrame(object sender, AForge.Video.NewFrameEventArgs eventArgs)
+        {
+            Bitmap bMap = new Bitmap(eventArgs.Frame, 800, 600);
+            try
+            {
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    bMap.Save(ms, ImageFormat.Jpeg);
+                    byte[] bytes = ms.ToArray();
+                    udpClient.Send(bytes, bytes.Length, consumerEndPoint);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.ResetColor();
+            }
+        }
+    }
+}
